b0968d398b86c76fa154e5c5434df3d3c210dd59
sherzod938818-lab <sherzod938818@gmail.com>
2026-06-12 09:39:30 +0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+    if (err) { res.writeHead(404); res.end('Not found'); return; }
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(data);
+  });
+});
+
+server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
